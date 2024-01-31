26e0e8bafde77b9c87345fb095c0303007d61ab7
Carlos Maiolino <cem@maiolino.me>
2024-01-31 19:50:38 +0100

    Use dynamic allocated memory for the cow struct

--- Basics/main.cpp
+++ Basics/main.cpp
@@ -3,21 +3,23 @@
 #include "cow.h"
 
 int main(void) {
-	struct cow my_cow("Hildy", 7, PET);
+	struct cow *my_cow;
+	my_cow = new cow("Hildy", 7, PET);
 
-	std::cout << my_cow.get_name() << " is a type - " <<
-		(int)my_cow.get_purpose() <<
+	std::cout << my_cow->get_name() << " is a type - " <<
+		(int)my_cow->get_purpose() <<
 		std::endl;
 
-	std::cout << my_cow.get_name() << " is " <<
-		my_cow.get_age() << " years old" <<
+	std::cout << my_cow->get_name() << " is " <<
+		my_cow->get_age() << " years old" <<
 		std::endl;
 
 
-	my_cow.set_age(24);
-	std::cout << my_cow.get_name() << " is NOW " <<
-		my_cow.get_age() << " years old" <<
+	my_cow->set_age(24);
+	std::cout << my_cow->get_name() << " is NOW " <<
+		my_cow->get_age() << " years old" <<
 		std::endl;
 
+	delete my_cow;
 	return 0;
 }
